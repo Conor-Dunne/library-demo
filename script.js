89a2f8e00e623086a84f5f2ce223c3cc0372a734
Conor Dunne <conordunne@outlook.ie>
2022-07-12 07:12:04 +0900

    Add from and js to add books to array and display on DOM

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 const mainContent = document.querySelector(".main-content");
+const addBookForm = document.querySelector(".add-book-form");
 
 let bookArray = [
     {
         title: "Book 1",
         author: "Conor Dunne",
         pages: 150,
-        read: false
     },
     {
         title: "Book 2",
         author: "Conor Dunne",
         pages: 150,
-        read: true
     }
 ]
 
+function Book(title,author,pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+
+function addBook () {
+    event.preventDefault();
+    const addTitle = document.querySelector("#book-title");
+    const newTitle = addTitle.value;
+    let narnia = new Book(newTitle, "test", 30);
+    bookArray.push(narnia);
+    postBook(narnia);
+    addTitle.value = "";
+    refreshDomBookList();
+    bookArray.forEach(postBook);
+}
+
+// const newBook = new Book("Bible", "God", 1000);
+// bookArray.push(newBook);
 
 
 const postBook = function (obj) {
@@ -23,9 +43,21 @@ const postBook = function (obj) {
     const title = document.createElement("h1");
     title.textContent = obj.title;
     bookDiv.appendChild(title);
+    const author = document.createElement("h2");
+    author.textContent = obj.author;
+    bookDiv.appendChild(author);
     mainContent.appendChild(bookDiv);
 }
 
+const refreshDomBookList = function() {
+    const allBooks = document.querySelectorAll(".book");
+    for (const el of allBooks) {
+        el.remove();
+    }
+};
+
 bookArray.forEach(postBook);
+
+addBookForm.addEventListener("submit", addBook);
 
 
