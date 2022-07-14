0e795758e96dc2d0e00c549bd7b0405a5d66ea23
cdutd83 <cdutd83@gmail.comasd>
2022-07-14 14:07:59 +0900

    Add delete buttons on books, and function to delete from array. Need to fix index numbers and to delete correct books

--- script.js
+++ script.js
@@ -1,53 +1,17 @@
-const mainContent = document.querySelector(".main-content");
-const addBookForm = document.querySelector(".add-book-form");
-
 let bookArray = [
     {
         title: "Book 1",
         author: "Conor Dunne",
         pages: 150,
-    },
-    {
-        title: "Book 2",
-        author: "Conor Dunne",
-        pages: 150,
-    },
-    {
-        title: "Book 3",
-        author: "Conor Dunne",
-        pages: 150,
-    },
-    {
-        title: "Book 4",
-        author: "Conor Dunne",
-        pages: 150,
-    },
-    {
-        title: "Book 5",
-        author: "Conor Dunne",
-        pages: 150,
-    },
-    {
-        title: "Book 6",
-        author: "Conor Dunne",
-        pages: 150,
-    },
-    {
-        title: "Book 7",
-        author: "Conor Dunne",
-        pages: 150,
-    },
-    {
-        title: "Book 8",
-        author: "Conor Dunne",
-        pages: 150,
-    },
-    {
-        title: "Book 9",
-        author: "Conor Dunne",
-        pages: 150,
+        index: 0
     }
 ]
+
+const mainContent = document.querySelector(".main-content");
+const addBookForm = document.querySelector(".add-book-form");
+
+
+
 
 function Book(title,author,pages) {
     this.title = title;
@@ -58,29 +22,40 @@ function Book(title,author,pages) {
 
 function addBook () {
     event.preventDefault();
-    const addTitle = document.querySelector("#book-title");
-    const newTitle = addTitle.value;
-    let narnia = new Book(newTitle, "test", 30);
-    bookArray.push(narnia);
-    postBook(narnia);
-    addTitle.value = "";
+    const newBookTitle = document.querySelector("#book-title");
+    const authorName = document.querySelector("#book-author");
+    const noOfpages = document.querySelector("#book-pages"); 
+    let newBook = new Book(newBookTitle.value, authorName.value, noOfpages.value);
+    newBook.index = bookArray.length;
+    bookArray.push(newBook);
+    postBook(newBook);
+    newBookTitle.value = "";
+    authorName.value = "";
+    noOfpages.value = "";
     refreshDomBookList();
     bookArray.forEach(postBook);
+    deleteButtons = Array.from(document.querySelectorAll(".delete-btn"));
+    deleteButtons.forEach(btn => btn.addEventListener("click", deleteBook));
+
 }
-
-// const newBook = new Book("Bible", "God", 1000);
-// bookArray.push(newBook);
-
 
 const postBook = function (obj) {
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
-    const title = document.createElement("h1");
-    title.textContent = obj.title;
-    bookDiv.appendChild(title);
-    const author = document.createElement("h2");
-    author.textContent = obj.author;
-    bookDiv.appendChild(author);
+    const titleH1 = document.createElement("h1");
+    titleH1.textContent = obj.title;
+    bookDiv.appendChild(titleH1);
+    const authorH2 = document.createElement("h2");
+    authorH2.textContent = obj.author;
+    bookDiv.appendChild(authorH2);
+    const pagesH3 = document.createElement("h3")
+    pagesH3.textContent = `No. of pages: ${obj.pages}`;
+    bookDiv.appendChild(pagesH3);
+    const deleteBtn = document.createElement("div");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    bookDiv.appendChild(deleteBtn);
+    bookDiv.dataset.index = obj.index;
     mainContent.appendChild(bookDiv);
 }
 
@@ -91,8 +66,28 @@ const refreshDomBookList = function() {
     }
 };
 
+const deleteBook = function () {
+    const bookIndex = this.parentElement.dataset.index;
+    
+    refreshDomBookList();
+    bookArray.forEach(postBook);
+    deleteButtons = Array.from(document.querySelectorAll(".delete-btn"));
+    deleteButtons.forEach(btn => btn.addEventListener("click", deleteBook));
+
+}
+
+
+
 bookArray.forEach(postBook);
 
+
 addBookForm.addEventListener("submit", addBook);
+
+
+let deleteButtons = Array.from(document.querySelectorAll(".delete-btn"));
+deleteButtons.forEach(btn => btn.addEventListener("click", deleteBook));
+
+
+
 
 
